4646de71b8565e0740307cc605564a5675bfcc12
Chrisatin <christian-972@hotmail.com>
2024-07-02 18:29:06 -0500

    update db_connection

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -3,5 +3,7 @@ $mysqli = new mysqli("localhost", "root", "123456789", "restaurante_proyecto");
 
 if ($mysqli->connect_error) {
     die("Error de conexión: " . $mysqli->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos.";
 }
 ?>
